e71fa1366b3d018a9d365847cd86a09f5ab1d422
maoxiaoxing <930649054@qq.com>
2021-05-23 20:54:45 +0800

    将虚拟dom 转换为真实dom

--- TinyReact/src/packages/mounNativeElement.js
+++ TinyReact/src/packages/mounNativeElement.js
@@ -1,19 +1,7 @@
-import mountElement from "./mountElement"
+import createDOMElement from "./createDOMElement"
 
 export default function mounNativeElement(virtualDOM, container) {
-  let newElement = null
-  if (virtualDOM.type === 'text') {
-    // 文本节点
-    newElement = document.createTextNode(virtualDOM.props.textContent)
-  } else {
-    // 元素节点
-    newElement = document.createElement(virtualDOM.type)
-  }
-
-  // 递归创建子节点
-  virtualDOM.children.forEach((child) => {
-    mountElement(child, newElement)
-  })
+  const newElement = createDOMElement(virtualDOM)
   // 将转换之后的 DOM 对象挂载到页面中
   container.appendChild(newElement)
 }
