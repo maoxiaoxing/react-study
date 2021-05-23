87a499881dce77b968c3fe4820e2a397fcb68bab
maoxiaoxing <930649054@qq.com>
2021-05-23 15:46:33 +0800

    babel显示tsx

---
+++ TinyReact/src/index.js
@@ -0,0 +1,21 @@
+import TinyReact from './packages'
+
+const virtualDOM = (
+  <div className="container">
+    <h1>你好 Tiny React</h1>
+    <h2 data-test="test">(编码必杀技)</h2>
+    <div>
+      嵌套1 <div>嵌套 1.1</div>
+    </div>
+    <h3>(观察: 这个将会被改变)</h3>
+    {2 == 1 && <div>如果2和1相等渲染当前内容</div>}
+    {2 == 2 && <div>2</div>}
+    <span>这是一段内容</span>
+    <button onClick={() => alert("你好")}>点击我</button>
+    <h3>这个将会被删除</h3>
+    2, 3
+    <input type="text" value="13" />
+  </div>
+)
+
+console.log(virtualDOM)
