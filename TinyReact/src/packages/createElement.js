b21c90904886db0a6b60dca0cccce8fe80892ead
maoxiaoxing <930649054@qq.com>
2021-05-23 16:07:46 +0800

    创建dom对象

--- TinyReact/src/packages/createElement.js
+++ TinyReact/src/packages/createElement.js
@@ -1,7 +1,18 @@
-export default function createElement(type, props, children) {
+export default function createElement(type, props, ...children) {
+  const childElements = [...children].reduce((result, child) => {
+    if (typeof child !== 'boolean' && child !== null) {
+      if (child instanceof Object) {
+        result.push(child)
+      } else {
+        result.push(createElement('text', { textContent: child }))
+      }
+    }
+    return result
+  }, [])
+
   return {
     type,
-    props,
-    children,
+    props: Object.assign({ children: childElements }, props),
+    children: childElements,
   }
 }
