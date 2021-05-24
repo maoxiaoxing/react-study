a7c9fb167ca853e7e7efd923f2b01b534ab10b94
maoxiaoxing <930649054@qq.com>
2021-05-24 09:14:22 +0800

    增加修改dom

--- TinyReact/src/index.js
+++ TinyReact/src/index.js
@@ -20,6 +20,30 @@ const virtualDOM = (
   </div>
 )
 
+const modifyDOM = (
+  <div className="container">
+    <h1>你好 Tiny React</h1>
+    <h2 data-test="test">(编码必杀技)</h2>
+    <div>
+      嵌套1 <div>嵌套 1.1</div>
+    </div>
+    <h3>(观察: 这个将会被改变)</h3>
+    {2 == 1 && <div>如果2和1相等渲染当前内容</div>}
+    {2 == 2 && <div>2</div>}
+    <span>这是一段被修改过的内容</span>
+    <button onClick={() => alert("你好")}>点击我</button>
+    <h3>这个将会被删除</h3>
+    2, 3
+    <input type="text" />
+  </div>
+)
+
+TinyReact.render(virtualDOM, root)
+
+setTimeout(() => {
+  TinyReact.render(modifyDOM, root)
+}, 2000)
+
 function Demo() {
   return <div>hello</div>
 }
