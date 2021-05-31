38f19e3d41f12861260fd73ba80babf835b441d1
maoxiaoxing <930649054@qq.com>
2021-05-31 09:20:32 +0800

    实现任务的调度逻辑

--- Fiber/src/react/Misc/CreateTaskQueue/index.js
+++ Fiber/src/react/Misc/CreateTaskQueue/index.js
@@ -1,8 +1,20 @@
 const CreateTaskQueue = () => {
   const taskQueue = []
   return {
+    /**
+     * 向任务队列中添加任务
+     * @param {*} item 任务
+     */
     push: item => taskQueue.push(item),
+    /**
+     * 从任务队列中获取任务
+     */
     pop: () => taskQueue.shift(),
+    /**
+     * 判断任务队列中是否还有任务
+     * @returns bool
+     */
+    isEmpty: () => taskQueue.length === 0
   }
 }
 
--- Fiber/src/react/reconciliation/index.js
+++ Fiber/src/react/reconciliation/index.js
@@ -1,6 +1,38 @@
 import { createTaskQueue } from "../Misc"
 
 const taskQueue = createTaskQueue()
+const subTask = null
+
+const getFirstTask = () =>{}
+
+const executeTask = (fiber) => {
+
+}
+
+const workLoop = () => {
+  // 如果子任务不存在 就去获取子任务
+  if (!subTask) {
+    subTask = getFirstTask()
+  }
+
+  // 当有任务的时候 且 延迟时间大于1
+  while (subTask && deadline.timeRamaining() > 1) {
+    subTask = executeTask(subTask)
+  }
+}
+
+/**
+ * 调度任务
+ * @param deadline 
+ */
+const performTask = (deadline) => {
+  // 执行任务
+  workLoop(deadline)
+  // 有任务的时候 或者 任务队列不为空 在浏览器空闲时间接着执行被中断的事件
+  if (subTask || !taskQueue.isEmpty()) {
+    requestIdleCallback(performTask)
+  }
+}
 
 export const render = (element, dom) => {
   // 向任务队列中添加任务
@@ -11,4 +43,6 @@ export const render = (element, dom) => {
     dom,
     props: { children: element },
   })
+  // 在浏览器空闲的时候执行任务
+  requestIdleCallback(performTask)
 }
