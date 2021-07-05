86b4530fb561b8b2f90a34fd0a2c284f7d0d6e61
maoxiaoxing <930649054@qq.com>
2021-07-05 22:26:26 +0800

    setState

--- react-demo/src/pages/hooks/useState.jsx
+++ react-demo/src/pages/hooks/useState.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import { Button } from 'antd'
 
-const UseState = () => {
+const UseState = (props) => {
   const [count, setCount] = useState(0)
+  const [person, setPerson] = useState({age: 18, name: 'maoxiaoxing'})
+  const [coin, setCoin] = useState(() => props.coin || 0)
 
   return (
     <div>
       <p>{count}</p>
       <Button onClick={() => setCount(count + 1)}>+ 1</Button>
+      <p>----------------</p>
+      <p>年龄：{person.age}名字：{person.name}</p>
+      <Button onClick={() => setPerson({...person, name: 'yangxiaoA'})}>setPerson</Button>
+      <p>----------------</p>
+      <p>{coin}</p>
+      <Button onClick={() => setCoin(coin + 1)}>+ 1</Button>
     </div>
   )
 }
