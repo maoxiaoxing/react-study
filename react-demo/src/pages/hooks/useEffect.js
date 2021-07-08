0ae6f74202ceff8c3ff40621d01286274fdf9c5d
maoxiaoxing <930649054@qq.com>
2021-07-08 08:28:50 +0800

    生命周期

--- react-demo/src/pages/hooks/useEffect.js
+++ react-demo/src/pages/hooks/useEffect.js
@@ -4,9 +4,29 @@ import { Button } from 'antd'
 const UseEffect = () => {
   const [count, setCount] = useState(0)
 
+  // componentDidMount componentDidUpdate
   useEffect(() => {
     console.log(count)
   })
+
+  // componentDidMount
+  useEffect(() => {
+    console.log('componentDidMount')
+  }, [])
+
+  // componentDidUpdate componentWillUnMount
+  useEffect(() => {
+    return () => {
+      console.log('componentDidUpdate componentWillUnMount')
+    }
+  })
+
+  // componentWillUnMount
+  useEffect(() => {
+    return () => {
+      console.log('componentWillUnMount')
+    }
+  }, [])
 
   return (
     <div>
