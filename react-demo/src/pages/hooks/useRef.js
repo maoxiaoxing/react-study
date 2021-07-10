f58517cf330df22f96ca268c06967562d9d324fe
maoxiaoxing <930649054@qq.com>
2021-07-10 22:59:52 +0800

    跨越生命周期的useRef

--- react-demo/src/pages/hooks/useRef.js
+++ react-demo/src/pages/hooks/useRef.js
@@ -1,16 +1,34 @@
-import React, { useRef } from 'react'
-import { Input } from 'antd'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Input } from 'antd'
 
 const UseRef = () => {
   const userName = useRef()
+  const timerRef = useRef()
+  const [count, setCount] = useState(0)
 
   const useNameChange = () => {
     console.log(userName)
   }
 
+  // let timer
+
+  const stopCount = () => {
+    clearInterval(timerRef.current)
+  }
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setCount((count) => {
+        return count + 1
+      })
+    }, 1000);
+  }, [])
+
   return (
     <div>
       <Input ref={userName} onChange={useNameChange}></Input>
+      {count}
+      <Button onClick={stopCount}>停止</Button>
     </div>
   )
 }
