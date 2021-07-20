62308e8dc9427e03969126c85642c268647567d6
maoxiaoxing <930649054@qq.com>
2021-07-20 08:51:22 +0800

    useState规则

--- react-demo/src/pages/demo/rule.js
+++ react-demo/src/pages/demo/rule.js
@@ -4,24 +4,27 @@ import React, { useEffect, useState } from 'react'
 let isMount = false
 
 const Rule = () => {
-  // const [name, setName] = useState('毛小星')
-  // const [age, setAge] = useState('18')
-  const [hobby, setHobby] = useState('吃小熊饼干，看动画片')
+  let name, age, hobby, setName, setAge, setHobby;
 
-  if (isMount) {
-    const [name, setName] = useState('毛小星')
-    const [age, setAge] = useState('18')
+  // console.log(isMount, 'isMount')
+
+  if (!isMount) {
+    // eslint-disable-next-line
+    [name, setName] = useState('毛小星');
+    // eslint-disable-next-line
+    [age] = useState('18');
+    isMount = true;
   }
+  [hobby, setHobby] = useState('吃小熊饼干，看动画片');
 
-  useEffect(() => {
-    isMount = true
-  }, [])
+  console.log(hobby, 'hobby');
+  console.log(age, 'age');
 
   return (
     <div>
-      <p>{name}</p>
-      <p>{age}</p>
-      <p>{hobby}</p>
+      { name ? <p>姓名：{name}</p> : null }
+      { age ? <p>年龄：{age}</p> : null }
+      <p>爱好：{hobby}</p>
       <Button onClick={() => setName('杨小A')}>修改姓名</Button>
     </div>
   )
