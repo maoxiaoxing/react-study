ae38b8e2e92e028b64b76752e0075d59051b36d8
maoxiaoxing <930649054@qq.com>
2021-08-06 08:16:38 +0800

    hooks demo

--- react-demo/src/pages/hooks/index.js
+++ react-demo/src/pages/hooks/index.js
@@ -31,28 +31,35 @@ import React, { useState, useEffect } from 'react'
 import { Button } from 'antd'
 
 const userMap = new Map([
-  ['9527', '毛小星'],
-  ['9528', '杨秘书'],
+  ['9527', { name: '毛小星' }],
+  ['9528', { name: '杨秘书' }],
 ])
 
 const Demo = () => {
+  const [count, setCount] = useState(0)
   const [id1, setName] = useState('9527')
   const [id2, setName] = useState('9528')
+  const [content, setContent] = useState('')
   
   const getName = (_id) => {
     return userMap.get(_id)
   }
 
   const makeFriend = (_id1, _id2) => {
+    const user1 = getName(_id1)
+    const user2 = getName(_id2)
 
-    return `${_name1} 和 ${_name2} 变成了好朋友`
+    return `${user1.name} 和 ${use2.name} 变成了好朋友`
   }
+
+  useEffect(() => {
+    makeFriend(id1, id2)
+  }, [id1, id2, count])
 
   return (
     <div>
-      <p>{name}</p>
-      <Button type="primary" onClick={() => setName('杨秘书')}>setName</Button>
       <p>{count}</p>
+      <p>{count && content}</p>
       <Button type="primary" onClick={() => setCount((_count) => _count + 1)}>increment</Button>
     </div>
   )
