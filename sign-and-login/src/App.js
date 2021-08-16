bdc2fdbf3e472ccde5da7af77a3cca6aa1d40b54
maoxiaoxing <930649054@qq.com>
2021-08-16 08:13:56 +0800

    导航

--- sign-and-login/src/App.js
+++ sign-and-login/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login'
+import Login from './components/Login/Login'
 
 function App() {
   return (
---
+++ sign-and-login/src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import style from './login.scss'
+
+const Login = (props) => {
+
+  return (
+    <div className={}>
+      <div className="">
+        <div>1</div>
+        <div>·</div>
+        <div>2</div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
