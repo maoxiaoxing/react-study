559a89f429528a0839205afb559c558d4c5897af
maoxiaoxing <930649054@qq.com>
2021-08-17 08:25:49 +0800

    导航

--- sign-and-login/src/components/Login/Login.js
+++ sign-and-login/src/components/Login/Login.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import style from './login.module.scss'
+import { useHistory } from 'react-router-dom'
 // import './login.scss'
 
+const Sign = {
+  SIGN_IN: 'sign_in',
+  SIGN_UP: 'sign_up',
+}
+
 const Login = (props) => {
+  const history = useHistory()
+
+  const signIn = () => {
+    console.log(history)
+    history.push(Sign.SIGN_IN)
+  }
+
+  const signUp = () => {
+    history.push(Sign.SIGN_UP)
+  }
 
   return (
     <div className={style.login}>
       <div className={style.nav}>
-        <div>1</div>
+        <div onClick={signIn}>登录</div>
         <div>·</div>
-        <div>2</div>
+        <div onClick={signUp}>注册</div>
       </div>
     </div>
   )
