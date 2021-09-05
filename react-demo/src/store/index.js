bdfe53b3884625fe05627ed1793ca2a3fe260bb1
maoxiaoxing <930649054@qq.com>
2021-09-05 11:42:17 +0800

    redux-thunk

--- react-demo/src/store/index.js
+++ react-demo/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import AllReducer from './reducers'
 import LoggerMiddleware from './middleware/logger'
-import ThunkMiddleware from './middleware/thunk'
+// import ThunkMiddleware from './middleware/thunk'
+import thunk from 'redux-thunk'
 
 export const store = createStore(AllReducer, applyMiddleware(
   LoggerMiddleware,
-  ThunkMiddleware,
+  thunk,
 ))
