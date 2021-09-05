15fbf1edfdf05820e6b74cc7da5c2d4567701cea
maoxiaoxing <930649054@qq.com>
2021-09-05 20:48:07 +0800

    redux-actions

--- react-demo/src/store/actions/count.js
+++ react-demo/src/store/actions/count.js
@@ -1,12 +1,17 @@
 import { INCREMENT, DECREMENT, INCREMENT_ASYNC } from "../const";
+import { createAction } from 'redux-actions'
 
-export const increment = payload => ({type: INCREMENT, payload});
-export const decrement = payload => ({type: DECREMENT, payload});
+// export const increment = payload => ({type: INCREMENT, payload});
+// export const decrement = payload => ({type: DECREMENT, payload});
+export const increment = createAction(INCREMENT)
+export const decrement = createAction(DECREMENT)
 
 // export const increment_async = (payload) => dispatch => {
 //   setTimeout(() => {
 //     dispatch(increment(payload))
 //   }, 2000);
 // }
+
+
 
 export const increment_async = payload => ({type: INCREMENT_ASYNC, payload});
--- react-demo/src/store/reducers/count.js
+++ react-demo/src/store/reducers/count.js
@@ -1,24 +1,37 @@
 import { INCREMENT, DECREMENT } from '../const'
+import { handleActions as createReducer } from 'redux-actions'
+import { increment, decrement } from '../actions/count'
+
 
 const initialState = {
   count: 0
 }
 
-const CounterReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        count: state.count + action.payload,
-      }
-    case DECREMENT:
-      return {
-        ...state,
-        count: state.count - action.payload
-      }
-    default: 
-      return state;
-  }
-}
+const handleIncrement = (state, action) => ({count: state.count + action.payload})
+const handleDecrement = (state, action) => ({count: state.count - action.payload})
+
+const CounterReducer = createReducer({
+  [increment]: handleIncrement,
+  [decrement]: handleDecrement,
+}, initialState)
 
 export default CounterReducer
+
+// const CounterReducer = (state = initialState, action) => {
+//   switch(action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         count: state.count + action.payload,
+//       }
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         count: state.count - action.payload
+//       }
+//     default: 
+//       return state;
+//   }
+// }
+
+// export default CounterReducer
